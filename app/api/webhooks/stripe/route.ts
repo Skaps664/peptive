c0cbf0221f3ac23aab489792f5012838a05d8013
Skaps664 <msudaisk664@gmail.com>
2026-01-17 19:03:03 +0500

    checkout fixed lil

--- app/api/webhooks/stripe/route.ts
+++ app/api/webhooks/stripe/route.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe';
 import axios from 'axios';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia',
+  apiVersion: '2025-12-15.clover',
 });
 
 // This is your Stripe webhook secret (get it from Stripe Dashboard → Webhooks)
